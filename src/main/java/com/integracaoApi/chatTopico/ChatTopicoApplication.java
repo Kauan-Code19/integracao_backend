90c077ce1fefebdf5ad0fc17c1de30c442a9cd6a
Kauan-Code19 <pkauprofissional@gmail.com>
2025-03-29 11:38:06 -0300

    primeiro commmit, projeto criado.

---
+++ src/main/java/com/integracaoApi/chatTopico/ChatTopicoApplication.java
@@ -0,0 +1,13 @@
+package com.integracaoApi.chatTopico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTopicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatTopicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integracaoApi/chatTopico/ChatTopicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integracaoApi.chatTopico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTopicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
